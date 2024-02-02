657d3a4d0755ee434795b24635544c5f7c5c9a41
Mayro <mayro.mmdev@gmail.com>
2024-02-02 18:04:41 -0300

    fix: add the React import

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';

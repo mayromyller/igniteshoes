0b9789bc230159e23790f6e31c7ca530c6a1b4e4
Mayro <mayro.mmdev@gmail.com>
2024-02-02 18:38:38 -0300

    config plugin of onesignal and dotenv plugin

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import React from "react";
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { LogLevel, OneSignal } from "react-native-onesignal";
 
-import { Routes } from './src/routes';
+import { ONESIGNAL_ID } from "@env";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Routes } from "./src/routes";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+OneSignal.initialize(ONESIGNAL_ID);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });

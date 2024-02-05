41ef5168e54613abce6c90eef8adbf2880628681
Mayro <mayro.mmdev@gmail.com>
2024-02-05 12:03:58 -0300

    add url image avatar in home header component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,4 @@
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 import { Heading, HStack, Icon, Text, VStack, Avatar } from "native-base";
 
 import userPhotoDefault from "../assets/userPhotoDefault.png";
@@ -7,7 +7,7 @@ export function HomeHeader() {
   return (
     <HStack pt={16} pb={5} px={8} bg="gray.600" alignItems="center">
       <Avatar
-        source={userPhotoDefault}
+        source={{ uri: "https://github.com/mayromyller.png" }}
         size={16}
         mr={4}
         borderWidth={2}
@@ -20,16 +20,11 @@ export function HomeHeader() {
         </Text>
 
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Rodrigo Gonçalves
+          Mayro Myller
         </Heading>
       </VStack>
 
-      <Icon
-        as={Feather}
-        name="log-out"
-        color="gray.200"
-        size={7}
-      />
+      <Icon as={Feather} name="log-out" color="gray.200" size={7} />
     </HStack>
   );
 }
